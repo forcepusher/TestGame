185bd43dc137b7dc60e262dd3c8c3e9dbd3708bb
forcepusher <bionitsoup@gmail.com>
2023-10-24 23:35:33 +0700

    Add touch detection

--- Assets/Source/Scripts/CrossPlatformInput.cs
+++ Assets/Source/Scripts/CrossPlatformInput.cs
@@ -16,11 +16,11 @@ namespace Faraway.TestGame
         public float HorizontalMovementDelta => _horizontalMovementDeltaTouch + _horizontalMovementDeltaMouse;
         public bool Jump => _jumpKeyboard || _jumpTouch;
 
-        private float _horizontalMovementDeltaTouch;
-        private float _horizontalMovementDeltaMouse;
+        private float _horizontalMovementDeltaTouch = Input.touches[0].deltaPosition.x;
+        private float _horizontalMovementDeltaMouse = Input.GetAxisRaw("Horizontal");
 
-        private bool _jumpTouch;
-        private bool _jumpKeyboard = Input.GetButtonDown("Jump");
+        private bool _jumpTouch = Input.touches[0].tapCount >= 1;
+        private bool _jumpKeyboard = Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0);
 
         private async void TickLoop()
         {
@@ -30,12 +30,22 @@ namespace Faraway.TestGame
 
                 if (_disposed)
                     return;
+
+
             }
         }
 
-        
+        private Vector2 AverageTouchPositionPixelCoordinates
+        {
+            get
+            {
+                Vector2 positionSum = Vector2.zero;
+                foreach (Touch touches in Input.touches)
+                    positionSum += touches.rawPosition;
 
-        
+                return positionSum / Input.touchCount;
+            }
+        }
 
         public void Dispose()
         {
