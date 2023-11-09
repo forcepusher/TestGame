d57868ddcb2bb1ce561e9538eac54e1f6404ef35
forcepusher <bionitsoup@gmail.com>
2023-11-09 19:14:38 +0700

    Add vegetation generator

---
+++ Assets/Source/Scripts/LevelGenerator/EnvironmentVegetationGenerator.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using Reflex.Attributes;
+using UnityEngine;
+
+namespace Faraway.TestGame
+{
+    public class EnvironmentVegetationGenerator : MonoBehaviour
+    {
+        private const float GenerationAheadDistance = 200f;
+
+        [SerializeField]
+        private List<VegetationPiece> _vegetationPieces;
+
+        private float _lastPieceZPosition;
+
+        private MainCamera _mainCamera;
+
+        [Inject]
+        public void Inject(MainCamera mainCamera)
+        {
+            _mainCamera = mainCamera;
+        }
+
+        private void Update()
+        {
+            float generationDistance = _mainCamera.transform.position.z + GenerationAheadDistance;
+
+            while (_lastPieceZPosition < generationDistance)
+            {
+                VegetationPiece piece = _vegetationPieces[Random.Range(0, _vegetationPieces.Count)];
+                float spacing = Random.Range(piece.SpacingMin, piece.SpacingMax);
+                Instantiate(piece.Prefab, new Vector3(0f, 0f, _lastPieceZPosition + spacing), Quaternion.Euler(0f, Random.Range(0, 360f), 0f));
+                _lastPieceZPosition += _lastPieceZPosition + spacing * 2f;
+            }
+        }
+    }
+}
---
+++ Assets/Source/Scripts/LevelGenerator/VegetationPiece.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace Faraway.TestGame
+{
+    [Serializable]
+    public struct VegetationPiece
+    {
+        public GameObject Prefab;
+        public float SpacingMin;
+        public float SpacingMax;
+    }
+}
