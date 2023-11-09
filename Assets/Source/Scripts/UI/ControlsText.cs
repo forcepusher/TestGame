d3a414271d5e320d8a96575426a23fd13237406e
forcepusher <bionitsoup@gmail.com>
2023-11-09 16:11:33 +0700

    Controls text

---
+++ Assets/Source/Scripts/UI/ControlsText.cs
@@ -0,0 +1,14 @@
+using Agava.WebUtility;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Faraway.TestGame
+{
+    public class ControlsText : MonoBehaviour
+    {
+        private void Awake()
+        {
+            GetComponent<Text>().text = Device.IsMobile ? "Swipe Left/Right/Up" : "Use A/D/Space keys";
+        }
+    }
+}
