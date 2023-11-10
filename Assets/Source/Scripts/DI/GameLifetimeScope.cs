2542c7ef092bded42752ffc744f0596d659cecf6
forcepusher <bionitsoup@gmail.com>
2023-11-11 01:31:18 +0700

    Remove Hello world code

--- Assets/Source/Scripts/DI/GameLifetimeScope.cs
+++ Assets/Source/Scripts/DI/GameLifetimeScope.cs
@@ -7,13 +7,7 @@ namespace Faraway.TestGame
     {
         protected override void Configure(IContainerBuilder builder)
         {
-            builder.Register<HelloWorldService>(Lifetime.Singleton).AsSelf();
-            //builder.Register<GamePresenter>(Lifetime.Singleton).As<GamePresenter, ITickable>();
-            builder.Register<GamePresenter>(Lifetime.Singleton).AsSelf();
-            builder.UseEntryPoints(Lifetime.Singleton, entryPoints =>
-            {
-                entryPoints.Add<GamePresenter>();
-            });
+            
         }
     }
 }
