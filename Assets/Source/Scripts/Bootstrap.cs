44b5210dd90d5400aad5df90e6d901b649fdbea8
forcepusher <bionitsoup@gmail.com>
2023-11-11 20:40:48 +0700

    Add bootstrap scene loading

---
+++ Assets/Source/Scripts/Bootstrap.cs
@@ -0,0 +1,15 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine.SceneManagement;
+using VContainer.Unity;
+
+namespace Faraway.TestGame
+{
+    public class Bootstrap : IAsyncStartable
+    {
+        public async UniTask StartAsync(CancellationToken cancellation)
+        {
+            await SceneManager.LoadSceneAsync("Game");
+        }
+    }
+}
