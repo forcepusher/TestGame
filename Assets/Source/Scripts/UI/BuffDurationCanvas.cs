3345041d3101eba5c21b23c6c06b07be94e30383
forcepusher <bionitsoup@gmail.com>
2023-11-11 23:02:42 +0700

    Dispose observables

--- Assets/Source/Scripts/UI/BuffDurationCanvas.cs
+++ Assets/Source/Scripts/UI/BuffDurationCanvas.cs
@@ -1,13 +1,16 @@
 using VContainer;
 using VContainer.Unity;
 using UniRx;
+using System;
 
 namespace Faraway.TestGame
 {
-    public class BuffDurationCanvas : ITickable, IStartable
+    public class BuffDurationCanvas : ITickable, IStartable, IDisposable
     {
         private BuffDurationCanvasView _buffDurationCanvasView;
         private IRunner _runner;
+
+        private CompositeDisposable _disposable = new();
 
         [Inject]
         public void Construct(IRunner runner, BuffDurationCanvasView buffDurationCanvasView)
@@ -18,13 +21,18 @@ namespace Faraway.TestGame
 
         public void Start()
         {
-            _runner.EffectBehaviors.ObserveAdd().Subscribe(_effectBehavior => _buffDurationCanvasView.AddEffectView(_effectBehavior.Value));
-            _runner.EffectBehaviors.ObserveRemove().Subscribe(_effectBehavior => _buffDurationCanvasView.RemoveEffectView(_effectBehavior.Value));
+            _runner.EffectBehaviors.ObserveAdd().Subscribe(_effectBehavior => _buffDurationCanvasView.AddEffectView(_effectBehavior.Value)).AddTo(_disposable);
+            _runner.EffectBehaviors.ObserveRemove().Subscribe(_effectBehavior => _buffDurationCanvasView.RemoveEffectView(_effectBehavior.Value)).AddTo(_disposable);
         }
 
         public void Tick()
         {
             _buffDurationCanvasView.UpdateEffectViews();
+        }
+
+        public void Dispose()
+        {
+            _disposable.Dispose();
         }
     }
 }
